8020f76f9efb40b552b1345d276abf28fe34f3f5
Maciej Maćkowiak <mmackowiak08@gmail.com>
2023-01-27 00:25:40 +0100

    Integration test with EmbeddedKafka

--- src/main/java/com/kafka/libraryeventproducer/controller/LibraryEventsController.java
+++ src/main/java/com/kafka/libraryeventproducer/controller/LibraryEventsController.java
@@ -11,7 +11,6 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.kafka.support.SendResult;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/test/java/com/kafka/libraryeventproducer/controller/LibraryEventControllerIntegrationTest.java
+++ src/test/java/com/kafka/libraryeventproducer/controller/LibraryEventControllerIntegrationTest.java
@@ -1,23 +1,60 @@
 package com.kafka.libraryeventproducer.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kafka.libraryeventproducer.domain.Book;
 import com.kafka.libraryeventproducer.domain.LibraryEvent;
 import com.kafka.libraryeventproducer.domain.LibraryEventType;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.HashMap;
+
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EmbeddedKafka(topics = {"library-events"}, partitions = 3)
+@TestPropertySource(properties = {"spring.kafka.producer.bootstrap-servers=${spring.embedded.kafka.brokers}",
+        "spring.kafka.admin.properties.bootstrap.servers=${spring.embedded.kafka.brokers}"})
 public class LibraryEventControllerIntegrationTest {
 
     @Autowired
     TestRestTemplate testRestTemplate;
 
+    @Autowired
+    EmbeddedKafkaBroker embeddedKafkaBroker;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    private Consumer<Integer, String> consumer;
+
+    @BeforeEach
+    void setUp() {
+        HashMap<String, Object> configs = new HashMap<>(KafkaTestUtils.consumerProps("group1", "true", embeddedKafkaBroker));
+        consumer = new DefaultKafkaConsumerFactory<>(configs, new IntegerDeserializer(), new StringDeserializer()).createConsumer();
+        embeddedKafkaBroker.consumeFromAllEmbeddedTopics(consumer);
+    }
+
+    @AfterEach
+    void tearDown() {
+        consumer.close();
+    }
+
     @Test
-    void postLibraryEvent_validInput_validOutputAndStatusCode() {
+    @Timeout(2)
+    void postLibraryEvent_validInput_validValueAndStatusCode() throws JsonProcessingException {
         Book book = Book.builder()
                 .bookId(1)
                 .bookName("Harry")
@@ -30,11 +67,17 @@ public class LibraryEventControllerIntegrationTest {
                 .book(book)
                 .build();
 
+        String libraryEventAsJson = objectMapper.writeValueAsString(libraryEvent);
+
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<LibraryEvent> request = new HttpEntity<>(libraryEvent, headers);
 
         ResponseEntity<LibraryEvent> exchange = testRestTemplate.exchange("/libraryevent", HttpMethod.POST, request, LibraryEvent.class);
-        Assertions.assertEquals(HttpStatus.CREATED,exchange.getStatusCode());
+        Assertions.assertEquals(HttpStatus.CREATED, exchange.getStatusCode());
+
+        ConsumerRecord<Integer, String> singleRecord = KafkaTestUtils.getSingleRecord(consumer, "library-events");
+        String value = singleRecord.value();
+        Assertions.assertEquals(libraryEventAsJson,value);
     }
 }
